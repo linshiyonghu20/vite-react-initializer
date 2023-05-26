9f02535bde5acd6d32b6a3da3c8ec5f03e450dd1
闫召宇 <yanzhaoyu@4paradigm.com>
2023-05-26 15:25:11 +0800

    feat(vite.config): 添加后端代理

--- vite.config.ts
+++ vite.config.ts
@@ -17,4 +17,13 @@ export default defineConfig({
       '@u': path.resolve(__dirname, 'src/utils'),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // 改为后端服务器地址
+        changeOrigin: true,
+        rewrite: (url) => url.replace(/^\/api/, ''), // 根据后端地址修改url
+      },
+    },
+  },
 });
